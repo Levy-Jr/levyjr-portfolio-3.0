39af0e7e4a3ff77dfff39337909376b4e87305b9
Levy-Jr <levybusinessjr@gmail.com>
2024-07-22 03:34:54 -0400

    fix add google search console metatag verification the right way

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Head from "next/head";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -8,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Levy Jr Portfolio",
   description: "Não é só um site, é uma experiência! Se você deseja aproveitar ao máximo as vantagens que a presença online pode oferecer, estou aqui para ajudar a transformar essa visão em realidade.",
+  verification: {
+    google: 'GAGdEvTt-eC51oiqkqLRCE6pVutnClRVYnI-pM47pRA'
+  }
 };
 
 export default function RootLayout({
@@ -17,9 +19,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="google-site-verification" content="GAGdEvTt-eC51oiqkqLRCE6pVutnClRVYnI-pM47pRA" />
-      </Head>
       <body
         className={`${inter.className}`}>
         {children}
