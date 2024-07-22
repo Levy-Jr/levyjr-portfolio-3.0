38bad4827e3a44708c5e4a1b8843219228f13816
Levy-Jr <levybusinessjr@gmail.com>
2024-07-22 03:19:59 -0400

    fix delete html serach console google verification and add metatag verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="GAGdEvTt-eC51oiqkqLRCE6pVutnClRVYnI-pM47pRA" />
+      </head>
       <body
         className={`${inter.className}`}>
         {children}
